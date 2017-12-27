22314b1768b49ffadae1d13d46911b61209433f9
TutorialStack <javedsarwa86@gmail.com>
2017-12-27 11:51:28 +0530

    integrated code for Google AdMob
    added all files, xml and java code for Google AdMob

---
+++ app/src/main/java/in/tutorialstack/googleadmob/MainActivity.java
@@ -0,0 +1,22 @@
+package in.tutorialstack.googleadmob;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, "ca-app-pub-7643266345625929~4795636158");
+
+        AdView mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
