e5c707c4b1b911b0793aa495a6c23869c0da5927
TutorialStack <javedsarwa86@gmail.com>
2017-12-28 00:12:12 +0530

    Added code for Interstitial Ads
    Step by step guide for us to Integrating Google AdMob Interstitial Ads to android application.

--- app/src/main/java/in/tutorialstack/googleadmob/MainActivity.java
+++ app/src/main/java/in/tutorialstack/googleadmob/MainActivity.java
@@ -2,11 +2,16 @@ package in.tutorialstack.googleadmob;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
+
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +20,27 @@ public class MainActivity extends AppCompatActivity {
 
         MobileAds.initialize(this, "ca-app-pub-7643266345625929~4795636158");
 
+        // Banner
         AdView mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+
+        // Interstitial
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-7643266345625929/3567501731");
+        AdRequest adInterstitialRequest = new AdRequest.Builder().build();
+        mInterstitialAd.loadAd(adInterstitialRequest);
+    }
+
+    public void showInterstitialAdd(View view) {
+        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        showInterstitialAdd(null);
+        super.onDestroy();
     }
 }
