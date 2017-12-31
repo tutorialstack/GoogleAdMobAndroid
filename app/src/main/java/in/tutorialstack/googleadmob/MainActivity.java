a578db1b323ad3de1702834743b23f4ce652b94f
TutorialStack <javedsarwa86@gmail.com>
2017-12-31 23:09:40 +0530

    Rewarded Ads
    added code for rewarded ads

--- app/src/main/java/in/tutorialstack/googleadmob/MainActivity.java
+++ app/src/main/java/in/tutorialstack/googleadmob/MainActivity.java
@@ -1,22 +1,30 @@
 package in.tutorialstack.googleadmob;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context context;
     private InterstitialAd mInterstitialAd;
+    private RewardedVideoAd mRewardedVideoAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
 
         MobileAds.initialize(this, "ca-app-pub-7643266345625929~4795636158");
 
@@ -30,17 +38,66 @@ public class MainActivity extends AppCompatActivity {
         mInterstitialAd.setAdUnitId("ca-app-pub-7643266345625929/3567501731");
         AdRequest adInterstitialRequest = new AdRequest.Builder().build();
         mInterstitialAd.loadAd(adInterstitialRequest);
+
+        //RewardedVideoAd
+        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
+        mRewardedVideoAd.loadAd("ca-app-pub-7643266345625929/7404149866", new AdRequest.Builder().build());
+        mRewardedVideoAd.setRewardedVideoAdListener(rewardedVideoAdListener);
     }
 
-    public void showInterstitialAdd(View view) {
+    public void showInterstitialAd(View view) {
         if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
             mInterstitialAd.show();
         }
     }
 
+    public void showRewardedAd(View view) {
+        if (mRewardedVideoAd != null && mRewardedVideoAd.isLoaded()) {
+            mRewardedVideoAd.show();
+        }
+    }
+
+    RewardedVideoAdListener rewardedVideoAdListener = new RewardedVideoAdListener() {
+        @Override
+        public void onRewardedVideoAdLoaded() {
+            Toast.makeText(context, "onRewardedVideoAdLoaded", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onRewardedVideoAdOpened() {
+            Toast.makeText(context, "onRewardedVideoAdOpened", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onRewardedVideoStarted() {
+            Toast.makeText(context, "onRewardedVideoStarted", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onRewardedVideoAdClosed() {
+            Toast.makeText(context, "onRewardedVideoAdClosed", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onRewarded(RewardItem rewardItem) {
+            String msg = "onRewarded! currency: " + rewardItem.getType() + "  amount: " + rewardItem.getAmount();
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onRewardedVideoAdLeftApplication() {
+            Toast.makeText(context, "onRewardedVideoAdLeftApplication", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onRewardedVideoAdFailedToLoad(int i) {
+            Toast.makeText(context, "onRewardedVideoAdFailedToLoad", Toast.LENGTH_SHORT).show();
+        }
+    };
+
     @Override
     protected void onDestroy() {
-        showInterstitialAdd(null);
+        showInterstitialAd(null);
         super.onDestroy();
     }
 }
